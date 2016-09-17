2b6d12210797e45696e0334351099a2a467e4985
miltonbrandes <miltonbrandess@hotmail.com>
2016-09-17 10:52:11 -0300

    inicio proyecto

---
+++ Aplicacion Desktop/ClinicaFrba/ClinicaFrba/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicaFrba
+{
+    public partial class formInicioSesion : Form
+    {
+        public formInicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void button_iniciar_sesion_Click(object sender, EventArgs e)
+        {
+            if (txtContrasenia.Text == null || txtContrasenia.Text == "" || txtUsuario.Text == null || txtUsuario.Text == "")
+            {
+                MessageBox.Show("Tiene que completar todos los campos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //fijarse en db si usuario correcto
+                this.Hide();
+                Form pantallaPrincipal = new main();
+                pantallaPrincipal.Show();
+            }
+        }
+    }
+}
