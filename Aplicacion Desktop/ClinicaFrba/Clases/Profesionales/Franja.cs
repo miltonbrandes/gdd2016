230a1d535d35b9f7adb7ead4ff3d797be2d69748
fedelway <fede21perez@hotmail.com>
2016-09-23 20:37:00 -0300

    Agrego minutos en franja

--- Aplicacion Desktop/ClinicaFrba/Clases/Profesionales/Franja.cs
+++ Aplicacion Desktop/ClinicaFrba/Clases/Profesionales/Franja.cs
@@ -11,6 +11,8 @@ namespace Clases
         public int Id { get; set; }
         public int Dia { get; set; }
         public int HoraInicio { get; set; }
+        public int MinutoInicio { get;set; }
         public int HoraFin { get; set; }
+        public int MinutoFin { get;set; }
     }
 }
