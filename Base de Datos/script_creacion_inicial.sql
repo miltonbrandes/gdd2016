5c32d80fa8ee2f2976dd7e08bfe16be5dd7a841e
miltonbrandes <miltonbrandess@hotmail.com>
2016-10-28 20:17:58 -0300

    Tratando de las 48hs

--- Base de Datos/script_creacion_inicial.sql
+++ Base de Datos/script_creacion_inicial.sql
@@ -1925,25 +1925,22 @@ SET NOCOUNT ON;
 /*EXECUTE NOT_NULL.Agregar_Franja_A_Todos_Los_Medicos
 GO*/
 go
-
+--drop procedure NOT_NULL.Franja_Agregar
+--exec NOT_NULL.Franja_Agregar 2, 30, 4, 08, 00, 12, 00
 CREATE PROCEDURE NOT_NULL.Franja_Agregar(@id_agenda int, @matricula int, @dia int, @hora_inicio int, @minuto_inicio int, @hora_fin int, @minuto_fin int)
 as begin
 	declare @totalMinutos int
 	set @totalMinutos = 0
 	declare @fecha_inicio datetime
 	declare @fecha_fin datetime
-
 	select @fecha_inicio = agenda_fecha_inicio, @fecha_fin = agenda_fecha_fin from agenda where agenda_id = @id_agenda
-
 	set @totalMinutos = -- Busco cant horas semanales ya registradas
 		isnull((select sum( (f.hora_fin*60 + f.minuto_fin) - (f.hora_inicio*60 + f.minuto_inicio) )
 			from NOT_NULL.franja_horaria f, NOT_NULL.agenda a, NOT_NULL.medicoXespecialidad m
 			where f.agenda_id = a.agenda_id and m.medxesp_agenda = a.agenda_id and m.medxesp_profesional = @matricula
-				and datepart(week, @fecha_inicio + @dia) = datepart(week, cast(a.agenda_fecha_inicio as datetime) + f.dia) 
+				and datepart(week, DATEADD(d, DATEDIFF(day,0, @fecha_inicio) /7*7, 0)+7) = datepart(week, DATEADD(d, DATEDIFF(day,0, a.agenda_fecha_inicio) /7*7, 0)+7) 
 			group by m.medxesp_profesional), 0)
-	--select @totalMinutos
 	set @totalMinutos = @totalMinutos + (@hora_fin*60 + @minuto_fin) - (@hora_inicio*60 + @minuto_inicio) -- Le agrego las nuevas horas
-	--select @totalMinutos
 	if @totalMinutos > 48*60  -- Si supera las 48hs retorno 1, sino lo agrego
 		RETURN(1)
 	else
